f06642bdba1e54444a71bb2429afdc092e37a0c3
Victor Raton <vfbraton@gmail.com>
2022-06-23 17:12:06 -0300

    separate test

--- request_test.go
+++ request_test.go
@@ -7,12 +7,37 @@ import (
 )
 
 func TestAsyncWorker(t *testing.T) {
+	t.Run("simple sucess only test", func(t *testing.T) {
+
+		var result chan goany.Response
+
+		go goany.RequestOne(result)
+
+		data := <-result
+
+		if data.Error != nil {
+			t.Fail()
+		}
+	})
+
+	t.Run("simple error only test", func(t *testing.T) {
+
+		var result chan goany.Response
+
+		go goany.RequestOne(result)
+
+		data := <-result
+
+		if data.Error == nil {
+			t.Fail()
+		}
+	})
 	t.Run("simple concurrent test", func(t *testing.T) {
 
 		var result chan goany.Response
 
 		go goany.RequestOne(result)
-		go goany.RequestOne(result)
+		go goany.RequestTwo(result)
 
 		data := <-result
 
